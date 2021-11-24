43b9dd84ed18da3e3ec8209871c61a25082d37e5
Catherine <katerinasheps@gmail.com>
2021-11-24 16:25:18 +0300

    Fixed generator, but there are same weight vertex in graph problem

--- SimulatedAnnealing/generator.cpp
+++ SimulatedAnnealing/generator.cpp
@@ -2,81 +2,126 @@
 #include <cstdlib>
 #include <fstream>
 #include <utility>
+#include <random>
+#include <cmath>
 
 #include "solution.h"
+
+std::vector<std::pair<oneJob *, std::pair<int, int>>> allJobs;
+	
+
+void print() {
+	for (size_t i = 0; i < allJobs.size(); i++) {
+		std::cout << "Num: " << allJobs[i].first->getNum() << " Weight: " << allJobs[i].first->getDuration();
+		std::cout << " Component " << allJobs[i].second.first << " Dependences: ";
+		if (allJobs[i].first->depNum.size()) {
+			for (auto j: allJobs[i].first->depNum) {
+				std::cout << j << " ";
+			}
+		}
+		std::cout << std::endl;
+
+	}
+}
 
 int distForWeight(int min, int max, int disp){
 	std::random_device rd{};
     std::mt19937 gen{rd()};
-    std::normal_distribution<int> dist{(max - min) / 2, disp}; 
-    int res = dist(gen);
+    std::normal_distribution<> dist{(max - min) / 2, disp}; 
+    int res = std::round(dist(gen));
     while (res < min || res > max) {
-    	res = dist(gen);
+    	res = std::round(dist(gen));
     }
     return res;
 }
 
-int distForDegree(int mean, int disp, int vertexNum){
+int distForComp(int mean, int disp, int vertexNum){
 	std::random_device rd{};
     std::mt19937 gen{rd()};
-    std::normal_distribution<int> dist{mean, disp}; 
-    int res = dist(gen);
+    std::normal_distribution<> dist{mean, disp}; 
+    int res = std::round(dist(gen));
     while (res < 0 || res > vertexNum) {
-    	res = dist(gen);
+    	res = std::round(dist(gen));
     }
     return res;
 }
 
-void compInitialize (std::vector<std::pair<oneJob *, int>>  *allJobs, int vertexNum, int compNum, int dispComp)
+
+int distForDegree(int mean, int disp, int min){
+	std::random_device rd{};
+    std::mt19937 gen{rd()};
+    std::normal_distribution<> dist{mean, disp}; 
+    int res = std::round(dist(gen));
+    while (res < min) {
+    	res = std::round(dist(gen));
+    }
+    return res;
+}
+
+
+void compInitialize ( int vertexNum, int compNum, int dispComp)
 {
 	int vertex = vertexNum, newCompVertexNum, j = 0;
 	while (vertex > compNum) {
-		newCompVertexNum = distForDegree(compNum, dispComp, vertex);
-		for (size_t i = vertexNum - vertex - newCompVertexNum; i < vertexNum - vertex; i++) {
-			*allJobs[i].second = j;
+		newCompVertexNum = distForComp(compNum, dispComp, vertex);
+		//std::cout << "newComp = " << newCompVertexNum << " Start = " << vertexNum - vertex << " Finish = " << vertexNum - vertex + newCompVertexNum << std::endl;
+		for (size_t i = vertexNum - vertex; i < vertexNum - vertex + newCompVertexNum; i++) {
+			allJobs[i].second.first = j;
 		}
 		j++;
 		vertex -= newCompVertexNum;
 	}
 	for (size_t i = vertexNum - vertex; i < vertexNum; i++) {
-		*allJobs[i].second = j;
+		allJobs[i].second.first = j;
 	}
 	return;
 }
 
-void degreeInitialize (std::vector<std::pair<oneJob *, int>>  *allJobs, int vertexDegree, int dispDegree)
+
+
+
+void degreeInitialize (int vertexDegree, int dispDegree)
 {
 	int j = 0, cur = 0, k = 0, last = 0;
 	while (true) {
 		k = 0;
 		for (size_t i = cur; i < allJobs.size(); i++) {
-			if (allJobs[i].second == j) {
+			if (allJobs[i].second.first == j) {
 				cur++;
 				k++;
 			} else {
 				break;
 			}
 		}
+		std::cout << "COMP " << j << " ELEM: " << k << " last " << last << " cur " << cur << std::endl;
 		for (size_t i = last; i < cur; i++) {
-			allJobs[i].first->depNum = std::vector(distForDegree(vertexDegree, dispDegree, k - (cur - last)), -1);
-			for (size_t ii = 0; ii < allJobs[i].first->depNum.size(); ii++) {
-				allJobs[i].first->depNum.push_back(allJobs[last + i + ii].first);
+			int depVertNum = distForDegree(vertexDegree, dispDegree, allJobs[i].second.second);
+			std::cout << "         VERT: " << i << " Degree: " << depVertNum << " DEP: " << allJobs[i].second.second << std::endl;
+			allJobs[i].first->depNum = std::vector<size_t>();
+			depVertNum -= allJobs[i].second.second;
+			std::cout << "              DEP: " << depVertNum << " MIN: " << i + 1 << " MAX: " << std::min((int)(i + depVertNum), cur) << std::endl;
+			for (size_t ii = i + 1; ii < std::min((int)(i + depVertNum + 1), cur); ii++) {
+				allJobs[i].first->depNum.push_back(allJobs[ii].first->getNum());
+				allJobs[ii].second.second ++;
 			}
 		}
 		j++;
 		last = cur;
+		if (cur >= allJobs.size()) {
+			break;
+		}
 	}
 	return;
 }
 
-void inputFormat (std::vector<oneJob *> allJobs, std::string filename) {
+void inputFormat (std::string filename) {
 	std::ofstream file;
   	file.open(filename + ".txt");
   	file << allJobs.size() << std::endl;
   	
   	for (auto job : allJobs) {
-  		file << job->getDuration();
-  		for (auto j: job->depNum) {
+  		file << job.first->getDuration();
+  		for (auto j: job.first->depNum) {
   			file << " " << j;
   		}
   		file << std::endl;
@@ -84,26 +129,27 @@ void inputFormat (std::vector<oneJob *> allJobs, std::string filename) {
   	return;
 }
 
-void dotFormat (std::vector<oneJob *> allJobs, std::string filename) {
+void dotFormat (std::string filename) {
 	std::ofstream file;
-  	file.open(filename + ".txt");
+  	file.open(filename + ".dot");
   	file << "digraph G {" << std::endl;
   	file << "    graph [fontsize=24]" << std::endl;
   	file << "    edge [fontsize=24]" << std::endl;
  	file << "    node [fontsize=24]" << std::endl;
   	file << "    ranksep = 1.5" << std::endl;
   	file << "    nodesep = .25" << std::endl;
-  	file << "    edge [style="setlinewidth(3)"];" << std::endl;
   	for (auto job : allJobs) {
-  		file << "    " << job->getDuration() << " -> { ";
-  		for (auto j: job->depNum) {
-  			file << j << " ";
+  		file << "    " << job.first->getDuration() << " -> { ";
+  		for (auto j: job.first->depNum) {
+  			file << allJobs[j].first->getDuration() << " ";
   		}
-  		file << "}" << std::endl;
+  		file << "};" << std::endl;
   		
   	}
   	file << "}" << std::endl;
 }
+
+
 
 int main (int argc, char * argv[]) 
 {
@@ -117,12 +163,20 @@ int main (int argc, char * argv[])
 	int compNum = std::atoi(argv[5]), dispComp = std::atoi(argv[6]);
 	int vertexDegree = std::atoi(argv[7]), dispDegree = std::atoi(argv[8]);
 
-	std::vector<std::pair<std::oneJob *, int>> jobVector;
 	for (size_t i = 0; i < vertexNum; i++) {
-		jobVector.push_back(std::make_pair(new oneJob (distForWeight(minWeight, maxWeight, distForWeight), i), -1));
+		allJobs.push_back(std::make_pair(new oneJob (distForWeight(minWeight, maxWeight, dispVertexWeight), i), std::make_pair(-1, 0)));
 		//jobVector[i].first->depNum = std::vector<size_t>(distForDegree(vertexDegree, dispDegree, vertexNum), -1);
 	}
-	compInitialize(&allJobs,  vertexNum, compNum, dispComp);
-	degreeInitialize(&allJobs, vertexDegree, distForDegree);
+
+
+	compInitialize(vertexNum, compNum, dispComp);
 	
+	print();
+	std::cout << "\n=================================\n" << std::endl;
+	
+	degreeInitialize(vertexDegree, dispDegree);
+	print();
+	inputFormat("in");
+	dotFormat("forGraph");
+	return 0;
 }
